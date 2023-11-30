e8b213514fca7abf2c2a4a19bb3f7f61ef7a12e9
Bruno Capobianco <bruno.capobianco@outlook.it>
2023-11-30 18:05:53 +0100

    xorbun
    added some improve

--- src/esercizio2/GestioneDipendenti.java
+++ src/esercizio2/GestioneDipendenti.java
@@ -1,20 +1,75 @@
 package esercizio2;
 import esercizio1.Dipendente;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class GestioneDipendenti
 {
     public static void main(String[] args)
     {
-            Dipendente utente1 = new Dipendente(233432, 24.4, 23, "operaio", "produzione");
-            Dipendente utente2 = new Dipendente(213212, 12.4, 21, "operaio", "produzione");
-            Dipendente utente3 = new Dipendente(432465, 43.2, 12, "dirigente", "vendite");
-            Dipendente utente4 = new Dipendente(4654465, 32.2, 8, "impiegato", "amministrazione");
-            utente1.promosso();
-            utente2.promosso();
-            utente3.promosso();
-            utente4.promosso();
-            System.out.println(utente1.getLivello());
-            System.out.println(utente2.getLivello());
-            System.out.println(utente3.getLivello());
-            System.out.println(utente4.getLivello());
+        Dipendente [] utenti=new Dipendente[5];
+        Random  rndm=new Random();
+        for(int i=0;i< utenti.length;i++)
+        {
+            int matricola=rndm.nextInt(1,10000);
+            double stipendio=rndm.nextDouble(1.0,10000.0);
+            int importostraordinari=rndm.nextInt(1,50);
+            String livello="";
+            String dipartimento="";
+            int selettore=rndm.nextInt(1,4);
+            switch (selettore)
+            {
+                case 1:
+                {
+                    livello="operaio";
+                    break;
+                }
+                case 2:
+                {
+                    livello="impiegato";
+                    break;
+                }
+                case 3:
+                {
+                    livello="quadro";
+                    break;
+                }
+                case 4:
+                {
+                     livello="dirigente";
+                    break;
+                }
+            }
+            int selezionadipartimento=rndm.nextInt(1,3);
+            switch (selezionadipartimento)
+            {
+                case 1:
+                {
+                     dipartimento="produzione";
+                    break;
+                }
+                case 2:
+                {
+                     dipartimento="amministrazione";
+                    break;
+                }
+                case 3:
+                {
+                     dipartimento="vendite";
+                    break;
+                }
+            }
+            utenti[i]=new Dipendente(matricola,stipendio,importostraordinari,livello,dipartimento );
+        }
+       for(int i=0;i< utenti.length;i++)
+       {
+           utenti[i].stampadati();
+       }
+       for(int i=0;i< utenti.length;i++)
+       {
+           utenti[i].promosso();
+           System.out.println(utenti[i].getLivello());
+       }
     }
 }
